01af66e248be411cca9a1750cd2f17110df7eb5f
joshua adewale <61548156+jossi47@users.noreply.github.com>
2021-03-27 15:32:35 +0200

    updating

--- script.js
+++
@@ -1,8 +0,0 @@
-  var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 5.2,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
